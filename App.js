ecbcf213c84e370576c9962098af8c8ab71f5ebd
junaidsheroz <junaidsheroz213@gmail.com>
2023-09-05 17:56:05 +0530

    a

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+// setup redux
+
 
 export default function App() {
   return (
